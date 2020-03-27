88a8438774fdfce273c0c0b8473f8bbbcd440c01
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-03-27 11:24:01 -0400

    adds first controller test

--- test/dummy/test/controllers/posts_controller_test.rb
+++ test/dummy/test/controllers/posts_controller_test.rb
@@ -4,4 +4,13 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'correctly authorizes a user' do
+    post = Post.first
+    user = User.first
+
+    get "/posts/#{post.id}", headers: {"xUser-Id": user.id}
+    puts @response.body.class
+    assert_equal post.to_json, @response.body
+  end
 end
