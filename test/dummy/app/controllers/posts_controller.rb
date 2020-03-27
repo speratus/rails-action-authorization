95b30e29db1b72b018e702af95e179b562a07179
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-03-27 10:44:57 -0400

    adds controller actions for test

--- test/dummy/app/controllers/posts_controller.rb
+++ test/dummy/app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+
+    def show
+        post = check_authorization(Post.find_by(id: params[:id]), current_user)
+        render json: post
+    end
+
+    private
+
+    def current_user
+        user = User.find_by(id: request.headers['xUser-Id'])
+    end
 end
