73fc98c2f3d8f57bc2266b29de0ab512bee4eb0d
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-03-27 11:42:02 -0400

    adds test for posts#index

--- test/dummy/test/controllers/posts_controller_test.rb
+++ test/dummy/test/controllers/posts_controller_test.rb
@@ -13,4 +13,12 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     puts @response.body.class
     assert_equal post.to_json, @response.body
   end
+
+  test 'correctly authorizes many entries' do
+    posts = Post.all
+    user = User.first
+
+    get posts_url, headers: {"xUser-Id": user.id}
+    assert_equal posts.to_json, @response.body
+  end
 end
