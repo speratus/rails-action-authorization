33c1ca81a642b7532a8bf3eebfe1caff72a4a430
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-11 14:52:09 -0400

    adds some more docstrings

--- lib/authorizer/active_record_patch.rb
+++ lib/authorizer/active_record_patch.rb
@@ -6,6 +6,10 @@ module ActionAuthorization
     # have to interact with these methods on concrete models and not by
     # interacting with ActiveRecord::Base directly.
 
+    ##
+    # returns the hash mapping permission rules to executable actions.
+    # This is used internally and should not need to be called directly
+    # by the user.
     def self.get_perms
       unless (self.class_variables.include?(:'@@perms'))
         @@perms = {}
@@ -14,15 +18,34 @@ module ActionAuthorization
       return @@perms
     end
     
+    ##
+    # Ensures that the +@@fallback_rule+ variable is defined.
+    # Used internally. There should be no need for users to call this method directly.
     def self.init_fallback_rule
       @@fallback_rule = nil unless (self.class_variable_defined?(:@@fallback_rule))
     end
-
+    
+    ##
+    # Defines an authorization rule for the specified
+    # action names. If multiple names are passed, then the same rule
+    # will be used for all of them.
+    # 
+    # Action names should take the following format "controller_name#action_name".
+    # E.G. To specify a rule for the update action on the posts controller, you would write
+    # 'posts#update'.
+    #
+    # names can also be symbols.
     def self.define_rule(*names, &block)
       perms = self.get_perms
       names.each {|name| perms[name.to_sym] = block}
     end
-
+    
+    ##
+    # Defins a fallback rule. The fallback rule defined by this
+    # class method will be used in every case where a permission rule is not
+    # specified. This is intended to be used in situations where 
+    # users wish to define some generic authorization check that will be run for
+    # every action that doesn't have its own rule specified.
     def self.set_fallback_rule(&rule)
       @@fallback_rule = rule
     end
